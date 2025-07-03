83b15b4ef1dbcf25a4d4c4bf69905ced33b59c34
Jaden Rotter <jaden.rotter@tum.de>
2025-07-03 14:30:08 +0200

    c

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -4,14 +4,14 @@ import React from "react";
 
 const rules = [
   "Each Constructor must have a name and a Racing Team Logo (RTL).",
-  "Each GP is run at 150cc.",
-  "No driving aids. That is, no steering aid and no auto-acceleration.",
+  "Each GP is run at 420cc.",
+  "No driving aids. That is, no steering aid and no auto-acceleration. Except for those who bribe the officials",
   "Performance Enhancing Drugs are a must.",
-  "You are allowed to bring one upgrade to your car each grand prix but no more than that, the set-up must be the same up to a given upgrade.",
+  "You are allowed to bring one upgrade to your car each grand prix but no more than that, the set-up must be the same up to a given upgrade. Again bribes are accepted pay-pal/jaden :)",
   "Missing a grand-prix results in a W.O (Walk over) unless a new time is agreed upon by both teams.",
   "GP:s follow the Official Mario Kart Calendar.",
   "Bot difficulty is set to the hardest.",
-  "If we can agree (democratically) on new rules or changing of the current ones we may do so.",
+  "If we can agree (authoritatively) on new rules or changing of the current ones we may do so.",
   "A team can have reserve drivers, although it is not beneficial towards the drivers championship.",
 ];
 
